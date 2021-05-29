9bf2dcdfbcda818c7ecb77657c854888a002b2e9
Jerry S Joseph <jerin.sebastian153@gmail.com>
2021-05-29 19:43:17 +0530

    removed client ID and Client SECRET

--- app/src/main/java/com/jstechnologies/internshalanotesapp/MyApp.java
+++ app/src/main/java/com/jstechnologies/internshalanotesapp/MyApp.java
@@ -4,18 +4,21 @@ import android.app.Application;
 
 import com.jstechnologies.usermanagement.UserManagement;
 
+//*
+// Base application class*/
 public class MyApp extends Application {
 
     private static MyApp mInstance;
 
     public static synchronized MyApp getInstance(){
-
         return mInstance;
     }
     @Override
     public void onCreate() {
         super.onCreate();
         mInstance=this;
-        UserManagement.Init("518136382026-dr1ovv7hrnvkb09teda54liocflvkimo.apps.googleusercontent.com","BY2bAZWhe95QFIQ7JbMXJ4rv");
+
+        //intialiizing user management engine
+        UserManagement.Init("YOUR_CLIENT_ID","YOUR_CLIENT_SECRET");
     }
 }
